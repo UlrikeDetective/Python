f9bdb7af117b64ac6f61f678b9eae5949444e937
Ulrike Herold <142745875+UlrikeDetective@users.noreply.github.com>
2024-04-22 19:38:39 +0200

    Update vowel remover function.py

--- vowel remover function.py
+++ vowel remover function.py
@@ -4,4 +4,4 @@ def vowel_remover(text):
         if l.lower() not in "aeiou":
             string += l
     return string
-print(vowel_remover("hello world!"))
+print(vowel_remover("hello world! Something Somewhere Somehow"))
